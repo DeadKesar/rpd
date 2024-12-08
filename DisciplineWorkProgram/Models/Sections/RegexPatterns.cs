4991b4c0bce86a533a831716f2badcd0d470e4b5
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2024-12-09 00:41:44 +0300

    Поправил регекс.

--- DisciplineWorkProgram/Models/Sections/RegexPatterns.cs
+++ DisciplineWorkProgram/Models/Sections/RegexPatterns.cs
@@ -5,7 +5,7 @@ namespace DisciplineWorkProgram.Models.Sections
 	public class RegexPatterns
 	{
 		//Наименование компетенции
-		public static readonly Regex CompetenceName = new Regex(@"^(УК-\d{1,2}.? )|^(ОПК-\d{1,2}.? )|^(ПК-\d{1,2}.? )");
+		public static readonly Regex CompetenceName = new Regex(@"^(УК-\d{1,2}.?(! )?(?!\d))|^(ОПК-\d{1,2}.?(! )?(?!\d))|^(ПК-\d{1,2}.?(! )?(?!\d))");
 		//Любая строка, содержащая в себе информацию о компетенции
 		public static readonly Regex Competence = new Regex(@"^(УК-\d)|^(ОПК-\d)|^(ПК-\d)");
 		//Поиск в строке ...?
